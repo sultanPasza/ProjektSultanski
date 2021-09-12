be22403ce620f0c53a28ac2c08e78d02fcd8f030
sultanPasza <wieckowskis000@gmail.com>
2021-09-12 20:34:37 +0200

    A

--- PROJECTHELLOWORLD/PROJECTHELLOWORLD.cpp
+++ PROJECTHELLOWORLD/PROJECTHELLOWORLD.cpp
@@ -3,20 +3,80 @@
 
 #include <iostream>
 
-int main()
+using namespace std;
+
+
+int main2(void)
+
 {
-    std::cout << "Hello World2!\n";
+	cout<<"Test dzia³ania VS.\nPies Cie jebal\a";
+
+	return 0;
 
 }
-// huhuhiuhiuyifyhgfjhgfhjgf
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main3(void)
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+{
+	cout << "To ja Twoj komputer";
+
+	return 0;
+}
+
+int main4(void)
+
+{
+	cout << "Czy\n";
+	cout << 3 + 4;
+	cout << "\njest wieksze od\n";
+	cout << "siedem";
+	return 0;
+}
+
+int main5(void)
+
+{
+	cout << "Czy\n" << 3 + 4 << "\njest wieksze od\n" << "siedem";
+	return 0;
+}
+
+int main6()
+
+{
+	int to_jest_zmienna;
+	unsigned int tylkoDodatnie;
+
+	float zmiennoprzecinkowa;
+
+	char jeden_znak;
+	unsigned char rowniez_znak;
+
+	int abc = -53;
+
+	tylkoDodatnie = 22;
+	zmiennoprzecinkowa = 12.42;
+	rowniez_znak = 'c';
+	to_jest_zmienna = 235252;
+	jeden_znak = 23;
+	
+	cout << "Wypisujemy zmienne:\n";
+	cout << "\nto_jest_zmienna = " << to_jest_zmienna ;
+	cout << "\ntylkoDodatnie = " << tylkoDodatnie ;
+	cout << "\nabc = " << abc;
+	cout << "\nzmiennoprzecinkowa = " << zmiennoprzecinkowa ;
+	cout << "\njeden_znak = " << jeden_znak ;
+	cout << "\nrowniez_znak = " << rowniez_znak ;
+
+	return 0;
+}
+
+int main()
+
+{
+	int nieznana;
+
+	nieznana = 123;
+
+	cout << "Liczba = " << nieznana;
+	return 0;
+}
